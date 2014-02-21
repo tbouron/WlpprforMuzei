38108d139f1e1e4a32b41874867e967f2103b5e8
tbouron <tbouron@gmail.com>
2014-02-21 11:15:10 +0000

    Updated copyright comment blocks + Javadoc comments

--- app/src/main/java/com/zenstyle/muzei/wlppr/DownloadReceiver.java
+++ app/src/main/java/com/zenstyle/muzei/wlppr/DownloadReceiver.java
@@ -1,3 +1,19 @@
+/*
+ * Copyright 2014 Thomas Bouron.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package com.zenstyle.muzei.wlppr;
 
 import android.app.DownloadManager;
@@ -8,12 +24,16 @@ import android.net.Uri;
 import android.os.Bundle;
 
 /**
- * Created by tbouron on 20/02/14.
+ * A broadcast receiver used to download wallpapers from wlppr.com into the default download folder
+ * on the current device.
  */
 public class DownloadReceiver extends BroadcastReceiver {
 
+    /** Action filter to activate this receiver */
     public static final String ACTION_DOWNLOAD = "com.zenstyle.muzei.wlppr.DOWNLOAD";
+    /** Extra string used to store the wallpaper URL */
     public static final String URL = "url";
+    /** Extra string used to store the wallpaper title */
     public static final String TITLE ="title";
 
     @Override
--- app/src/main/java/com/zenstyle/muzei/wlppr/WlpprService.java
+++ app/src/main/java/com/zenstyle/muzei/wlppr/WlpprService.java
@@ -1,22 +1,53 @@
+/*
+ * Copyright 2014 Thomas Bouron.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package com.zenstyle.muzei.wlppr;
 
 import java.util.List;
 
+import retrofit.RetrofitError;
 import retrofit.http.GET;
 import retrofit.http.Query;
 
 /**
- * Created by tbouron on 20/02/14.
+ * A service interface representing available endpoints for wlppr.com. This interface is intended to
+ * be used by a {@link retrofit.RestAdapter} instance.
  */
 public interface WlpprService {
 
+    /**
+     * Returns the last N wallpapers available.
+     *
+     * @param number The number of wallpaper to return (Optional, can be null)
+     * @return A {@link WlpprResponse}
+     * @throws RetrofitError if something goes wrong during the request
+     */
     @GET("/ajax/pile")
-    WlpprResponse getWallPaper(@Query("number") Integer number);
+    WlpprResponse getWallPapers(@Query("number") Integer number) throws RetrofitError;
 
+    /**
+     * A object representing the returned response from wlppr.com
+     */
     static class WlpprResponse {
         List<Wallpaper> wallpapers;
     }
 
+    /**
+     * A underlying object representing a returned wallpaper from wlppr.com
+     */
     static class Wallpaper {
         int id;
         String RGBColor;
